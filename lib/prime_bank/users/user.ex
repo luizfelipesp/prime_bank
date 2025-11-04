293091fb75dc05cced8b0c2f2a0f815c6ef0c59c
LuizF6856 <luizfellipe6843@gmail.com>
2025-11-04 18:39:23 -0300

    [S5-52] password virtual field to use for argon2 encrypt

--- lib/prime_bank/users/user.ex
+++ lib/prime_bank/users/user.ex
@@ -2,10 +2,13 @@ defmodule PrimeBank.Users.User do
   use Ecto.Schema
   import Ecto.Changeset
 
-  @required_params [:name, :password_hash, :email, :cep]
+  alias Ecto.Changeset
+
+  @required_params [:name, :password, :email, :cep]
 
   schema "users" do
     field :name, :string
+    field :password, :string, virtual: true
     field :password_hash, :string
     field :email, :string
     field :cep, :string
@@ -20,5 +23,12 @@ defmodule PrimeBank.Users.User do
     |> validate_length(:cep, is: 8)
     |> validate_format(:email, ~r/@/)
     |> validate_length(:name, min: 3)
+    |> add_password_hash()
   end
+
+  defp add_password_hash(%Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, password_hash: Argon2.hash_pwd_salt(password))
+  end
+
+  defp add_password_hash(changeset), do: changeset
 end
