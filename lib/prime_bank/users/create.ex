863c3f5db3e1cd48b71a52a9fef58ec54d277aae
luizfelipesp <luizfellipe6843@gmail.com>
2026-02-15 03:49:22 -0300

    [feature/credo_lib] fix credo issues from main merge

--- lib/prime_bank/users/create.ex
+++ lib/prime_bank/users/create.ex
@@ -1,9 +1,8 @@
 defmodule PrimeBank.Users.Create do
   @moduledoc false
-  alias PrimeBank.ViaCep.Client
   alias PrimeBank.Repo
   alias PrimeBank.Users.User
-  alias PrimeBank.ViaCep
+  alias PrimeBank.ViaCep.Client
 
   def call(%{"cep" => cep} = params) do
     with {:ok, _} <- client().call(cep) do
--- lib/prime_bank/via_cep/behaviour.ex
+++ lib/prime_bank/via_cep/behaviour.ex
@@ -1,3 +1,7 @@
 defmodule PrimeBank.ViaCep.Behaviour do
+  @moduledoc """
+  Defines the contract for viacep integration.
+  """
+
   @callback call(String.t()) :: {:ok, map()} | {:error, :atom}
 end
