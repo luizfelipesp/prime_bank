54d1dbabaf1dc2f972e3216dcfd408598871e4e4
luizfelipesp <luizfellipe6843@gmail.com>
2026-02-16 16:13:38 -0300

    add context of try

--- config/dev.exs
+++ config/dev.exs
@@ -53,7 +53,12 @@ config :prime_bank, PrimeBankWeb.Endpoint,
 config :prime_bank, dev_routes: true
 
 # Do not include metadata nor timestamps in development logs
-config :logger, :default_formatter, format: "[$level] $message\n"
+config :logger,
+  #  :default_formatter,
+  level: :info,
+  backend: [:console]
+
+#  format: "[$level] $message\n"
 
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
